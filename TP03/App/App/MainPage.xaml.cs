d40868f3084ab3c917681739cb3bdd83afbeafca
Josuel Santos <josuel.santos@aluno.ifsp.edu.br>
2022-10-22 18:56:32 -0300

    TP03

---
+++ TP03/App/App/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Login();
+        }
+
+        public async void Login()
+        {
+            var httpClient = new HttpClient(GetInsecureHandler());
+            var response = await httpClient.GetStringAsync("https://10.0.2.2:44359/api/Logins1");
+            var login = JsonConvert.DeserializeObject<List<Login>>(response);
+            ListLogin.ItemsSource = login;
+        }
+        void btnReload(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        public HttpClientHandler GetInsecureHandler()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                if (cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            };
+            return handler;
+        }
+    }
+}
---
+++ TP03/Web/Data/WebdbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Web.Data
+{
+    public class WebdbContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WebdbContext() : base("name=WebdbContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Web.Models.Login> Logins { get; set; }
+    }
+}
